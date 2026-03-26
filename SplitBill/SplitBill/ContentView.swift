03d27d1cdfa31e066aaf3778f4844f5907900fe3
bluga404 <walkervalentinussimanjuntak@gmail.com>
2026-03-26 10:34:27 +0700

    add project1: WeSplit

---
+++ SplitBill/SplitBill/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SplitBill
+//
+//  Created by Walker Valentinus Simanjuntak on 26/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SplitBill/SplitBill/SplitBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillApp.swift
+//  SplitBill
+//
+//  Created by Walker Valentinus Simanjuntak on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SplitBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
