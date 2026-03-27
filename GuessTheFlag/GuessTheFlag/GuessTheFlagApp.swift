882492822831134bbab96b22af1e03625f00ade9
bluga404 <walkervalentinussimanjuntak@gmail.com>
2026-03-27 15:19:41 +0700

    add project2-progress: UI

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Walker Valentinus Simanjuntak on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
